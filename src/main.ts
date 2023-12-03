d2af2a9ca2127ff0e945ff06fc04248f38ce4def
FB24M <103760918+iFB24M@users.noreply.github.com>
2023-12-04 00:36:13 +0400

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
 import './style.css'
-
-document.head.insertAdjacentHTML('beforeend', `
-<title>ай ем Nedodayn</title>
-<meta charset="UTF-8" />
-  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-`)
--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,7 @@
+import nunjucks from 'vite-plugin-nunjucks';
+
 export default {
+	plugins: [
+		nunjucks()
+	]
 }
